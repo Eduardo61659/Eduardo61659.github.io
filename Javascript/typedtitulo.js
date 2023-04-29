ef2437b97098e4fbda433cad1f645bacfb2210e0
Eduardo61659 <112517072+Eduardo61659@users.noreply.github.com>
2023-04-29 13:05:39 -0600

    commit initial

---
+++ Javascript/typedtitulo.js
@@ -0,0 +1,17 @@
+const typed = new Typed('.typed',{
+    strings: [
+        '<i class="palani"> Proyectos</i>',
+        '<i class="palani"> "¡Cuida el planeta, es el único hogar que tenemos!"</i>',
+    ],
+
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    shuffle: false,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html',
+});
